5e64898348f378a48acecacf1e124d9bb12bc4e2
ElperdonXV <davidemartinografico@gmail.com>
2021-12-01 17:09:22 +0100

    keep trying

--- js/script.js
+++ js/script.js
@@ -4,18 +4,22 @@
 //con difficoltà 3 => tra 1 e 49
 //Quando l’utente clicca su ogni cella, la cella cliccata si colora di azzurro.
 
+let select = document.getElementById('difficulty');
+let value = select.options[select.selectedIndex].value;
+
 
 //seleziono il livello di difficoltà
 const play = document.getElementById('play');
+console.log(play);
 play.addEventListener('click', function(){
-    let select = document.getElementById('difficulty');
-    let value = select.options[select.selectedIndex].value;
-    console.log(value);
+
 });
 
 
+
+
 //livello 1 
-if(value == 'easy'){
+if (value == 'easy') {
     const row = 10;
     const col = 10;
     const numberSquare = row * col;
@@ -25,6 +29,8 @@ if(value == 'easy'){
     for (let i = 0; i < numberSquare; i++) {
         const square = document.createElement('div');
         square.classList.add('square');
+        square.style.width = `calc(100% / ${col})`;
+        square.style.height = `calc(100% / ${row})`;
         square.append(i + 1);
         containerGrid.append(square);
     }
@@ -32,7 +38,7 @@ if(value == 'easy'){
 }
 
 //livello 2 
-if (value == 'normal') {
+else if (value == 'normal') {
     const row = 9;
     const col = 9;
     const numberSquare = row * col;
@@ -42,11 +48,32 @@ if (value == 'normal') {
     for (let i = 0; i < numberSquare; i++) {
         const square = document.createElement('div');
         square.classList.add('square');
+        square.style.width = `calc(100% / ${col})`;
+        square.style.height = `calc(100% / ${row})`;
         square.append(i + 1);
         containerGrid.append(square);
     }
 
 }
 
+
+//livello 3
+else if (value == 'hard') {
+    const row = 7;
+    const col = 7;
+    const numberSquare = row * col;
+    const maxNumber = numberSquare;
+    const minNumber = 1;
+    const containerGrid = document.getElementById('container');
+    for (let i = 0; i < numberSquare; i++) {
+        const square = document.createElement('div');
+        square.classList.add('square');
+        square.style.width = `calc(100% / ${col})`;
+        square.style.height = `calc(100% / ${row})`;
+        square.append(i + 1);
+        containerGrid.append(square);
+    }
+
+}
 
 
